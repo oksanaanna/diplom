8d57629c71ea14fedef08b820348c4b92611c94a
Oksana <oksanaanna@yahoo.com>
2018-09-26 21:09:51 +0300

    добавили список каталогов

--- src/main/java/com/example/demo/HomePage.java
+++ src/main/java/com/example/demo/HomePage.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("ALL")
 @Named
@@ -11,12 +15,24 @@ public class HomePage {
 
     @Inject
     private Person person;
+
     @Inject
     private CatalogRepository catalogRepository;
-    public String password;
+
+    private String password;
+
+    private Catalog selectedCatalog;
+
+    private List<SelectItem> catalogs;
+
+    private String login;
 
     private int age;
-    private String login;
+
+    @PostConstruct
+    public void init() {
+        catalogs = readCatalogsFromDatabase();
+    }
 
     public String checkPassword() {
         if (password.equals("pass")) {
@@ -27,18 +43,62 @@ public class HomePage {
         }
     }
 
+    private List<SelectItem> readCatalogsFromDatabase() {
+        List<SelectItem> catalogItems = new ArrayList<>();
+
+        for (Catalog catalog : catalogRepository.findAll()) {
+            SelectItem catalogItem = new SelectItem(catalog, catalog.getName());
+            catalogItems.add(catalogItem);
+        }
+
+        return catalogItems;
+    }
+
     public String age() {
         if (age < 10 || age > 100) {
             sendMessage("Your age is out of range");
             return null;
-        }else{
+        } else {
             return "goToMain";
         }
     }
-    public void sendMessage(String message){
-        FacesContext context= FacesContext.getCurrentInstance();
-        context.addMessage(null,new FacesMessage(FacesMessage.SEVERITY_WARN,
-                message,null));
+
+    public void sendMessage(String message) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+                message, null));
+    }
+
+    public List<SelectItem> getCatalogs() {
+        return catalogs;
+    }
+
+    public void setCatalogs(List<SelectItem> catalogs) {
+        this.catalogs = catalogs;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Catalog getSelectedCatalog() {
+        return selectedCatalog;
+    }
+
+    public void setSelectedCatalog(Catalog selectedCatalog) {
+        this.selectedCatalog = selectedCatalog;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public CatalogRepository getCatalogRepository() {
